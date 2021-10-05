a3b76f58c15120946e085e2110616536d018d93c
RoseGabriely <rosegabriely14@gmail.com>
2021-10-05 03:01:43 -0400

    created change operation action, and event handlers to change operations on click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import reducer, { initialState } from "./reducers";
-import { addOne, applyNumber } from "./actions";
+import { addOne, applyNumber, changeOperation } from "./actions";
 
 import "./App.css";
 
@@ -14,8 +14,12 @@ function App() {
   //   dispatch(addOne());
   // };
 
-  const eventHandler = (number) => {
+  const handleValues = (number) => {
     dispatch(applyNumber(number));
+  };
+
+  const handleOperation = (operator) => {
+    dispatch(changeOperation(operator));
   };
 
   return (
@@ -47,27 +51,27 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={() => eventHandler(1)} />
-              <CalcButton value={2} onClick={() => eventHandler(2)} />
-              <CalcButton value={3} onClick={() => eventHandler(3)} />
+              <CalcButton value={1} onClick={() => handleValues(1)} />
+              <CalcButton value={2} onClick={() => handleValues(2)} />
+              <CalcButton value={3} onClick={() => handleValues(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} onClick={() => eventHandler(4)} />
-              <CalcButton value={5} onClick={() => eventHandler(5)} />
-              <CalcButton value={6} onClick={() => eventHandler(6)} />
+              <CalcButton value={4} onClick={() => handleValues(4)} />
+              <CalcButton value={5} onClick={() => handleValues(5)} />
+              <CalcButton value={6} onClick={() => handleValues(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} onClick={() => eventHandler(7)} />
-              <CalcButton value={8} onClick={() => eventHandler(8)} />
-              <CalcButton value={9} onClick={() => eventHandler(9)} />
+              <CalcButton value={7} onClick={() => handleValues(7)} />
+              <CalcButton value={8} onClick={() => handleValues(8)} />
+              <CalcButton value={9} onClick={() => handleValues(9)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={() => handleOperation("+")} />
+              <CalcButton value={"*"} onClick={() => handleOperation("*")} />
+              <CalcButton value={"-"} onClick={() => handleOperation("-")} />
             </div>
 
             <div className="row ce_button">
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
+import React from "react";
+
+test("render without errors", () => {
+  render(<App />);
+});
